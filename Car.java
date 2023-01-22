92b7ed786dc8fdc5b22fe9125a41a4f3146fb2b7
Freja <corbermann@gmail.com>
2023-01-22 15:33:04 +0100

    Comments hopefully finished

--- Car.java
+++ Car.java
@@ -18,12 +18,37 @@ public abstract class Car implements Movable{
      */
     private double direction;
 
-    private final int nrDoors; // Number of doors on the car
-    double enginePower; // Engine power of the car
-    double currentSpeed; // The current speed of the car
-    private Color color; // Color of the car
-    private final String modelName; // The car model name
+    /**
+     * Number of doors that the car has.
+     */
+    private final int nrDoors;
+    /**
+     * Engine power of the car.
+     */
+    double enginePower;
+    /**
+     * The current speed of the car.
+     */
+    double currentSpeed;
+    /**
+     * Color of the car.
+     */
+    private Color color;
+    /**
+     * The car model name.
+     */
+    private final String modelName;
 
+    /**
+     * Basic constructor for a car.
+     * @param nrDoors Number of doors.
+     * @param color Color of the car.
+     * @param enginePower Engine power, also serves as max speed.
+     * @param name Model name for the car.
+     * @param xPosition Initial X position.
+     * @param yPosition Initial Y position.
+     * @param direction Initial direction.
+     */
     public Car(int nrDoors, Color color, double enginePower, String name,
                double xPosition, double yPosition, double direction) {
         this.nrDoors = nrDoors;
@@ -42,6 +67,11 @@ public abstract class Car implements Movable{
     public double getX() {
         return xPosition;
     }
+
+    /**
+     * Sets a new X position.
+     * @param xPosition New X position.
+     */
     public void setX(double xPosition) {
         this.xPosition = xPosition;
     }
@@ -52,6 +82,11 @@ public abstract class Car implements Movable{
     public double getY() {
         return yPosition;
     }
+
+    /**
+     * Sets a new Y position.
+     * @param yPosition New Y Position.
+     */
     public void setY(double yPosition) {
         this.yPosition = yPosition;
     }
@@ -62,51 +97,97 @@ public abstract class Car implements Movable{
     public double getDirection() {
         return direction;
     }
+
+    /**
+     * Sets a new direction.
+     * @param direction New direction.
+     */
     public void setDirection(double direction) {
         this.direction = direction;
     }
 
+    /**
+     * Gives the number of doors.
+     * @return Number of doors.
+     */
     public int getNrDoors(){
         return nrDoors;
     }
 
+    /**
+     * Gives the engine power.
+     * @return Engine power.
+     */
     public double getEnginePower(){
         return enginePower;
     }
 
+    /**
+     * Gives the current speed.
+     * @return Current speed.
+     */
     public double getCurrentSpeed(){
         return currentSpeed;
     }
 
+    /**
+     * Gives the name of the model.
+     * @return Name of the cars model.
+     */
     public String getModelName() {
         return modelName;
     }
 
+    /**
+     * Gives the color of the car.
+     * @return The cars current color.
+     */
     public Color getColor(){
         return color;
     }
 
+    /**
+     * Sets a new color for the car.
+     * @param clr The new color.
+     */
     public void setColor(Color clr){
         color = clr;
     }
 
+    /**
+     * Sets the current speed to 0.1.
+     */
     public void startEngine(){
         currentSpeed = 0.1;
     }
 
+    /**
+     * Sets the current speed to 0.
+     */
     public void stopEngine(){
         currentSpeed = 0;
     }
 
+    /**
+     * Clamp method, not sure where to put it.
+     * @param value A double to be clamped.
+     * @param lowerBound Lowerbound.
+     * @param upperBound Upperbound.
+     * @return Clamped value.
+     */
     public double clamp(double value, double lowerBound, double upperBound){
         return Math.max(lowerBound, Math.min(upperBound, value));
     }
+
+    /**
+     * SpeedFactor is used by each subclass to calculate speed when moving.
+     */
     abstract double speedFactor();
 
     /**
      * Sets a new higher currentSpeed value.
      * New value will be between 0 and enginePower.
-     * @param amount Should be between 1 and 0.
+     * @param amount Should be between 0 and 1.
      */
     public void incrementSpeed(double amount){
         currentSpeed = clamp(getCurrentSpeed() + speedFactor() * amount,0, enginePower);
@@ -115,7 +196,7 @@ public abstract class Car implements Movable{
     /**
      * Sets a new lower currentSpeed value.
      * New value will be between 0 and enginePower.
-     * @param amount Should be between 1 and 0.
+     * @param amount Should be between 0 and 1.
      */
     public void decrementSpeed(double amount){
         currentSpeed = clamp(getCurrentSpeed() - speedFactor() * amount,0, enginePower);
@@ -126,7 +207,7 @@ public abstract class Car implements Movable{
     /**
      * Increases the currentSpeed value.
      * Calls incrementSpeed with a value between 0 and 1.
-     * @param amount Should be between 1 and 0.
+     * @param amount Should be between 0 and 1.
      */
     public void gas(double amount){
         incrementSpeed(clamp(amount, 0 ,1));
@@ -137,7 +218,7 @@ public abstract class Car implements Movable{
     /**
      * Decreases the currentSpeed value.
      * Calls decrementSpeed with a value between 0 and 1.
-     * @param amount Should be between 1 and 0.
+     * @param amount Should be between 0 and 1.
      */
     public void brake(double amount){
         decrementSpeed(clamp(amount, 0, 1));
--- Saab95.java
+++ Saab95.java
@@ -1,26 +1,52 @@
 import java.awt.*;
-
+/**
+ * A class representing a Saab 95.
+ */
 public class Saab95 extends Car{
-
+    /**
+     * Boolean that affects the speed factor of the car.
+     */
     public boolean turboOn;
+
+    /**
+     * Constructor with parameters for initial position/direction.
+     *
+     * @param xPosition Initial X position.
+     * @param yPosition Initial Y position.
+     * @param direction Initial direction.
+     */
 
     public Saab95(double xPosition, double yPosition, double direction){
         super(2, Color.red, 125, "Saab95", xPosition, yPosition, direction);
 	    turboOn = false;
         stopEngine();
     }
+    /**
+     * Constructor with default arguments (0,0,0).
+     */
     public Saab95() {
         this(0,0,0);
     }
 
+    /**
+     * Turns turbo on.
+     */
     public void setTurboOn(){
 	    turboOn = true;
     }
 
+    /**
+     * Turns turbo off.
+     */
     public void setTurboOff(){
 	    turboOn = false;
     }
-    
+
+    /**
+     * Used to calculate new currentSpeed values.
+     *
+     * @return Factor in incrementSpeed and decrementSpeed.
+     */
     public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
