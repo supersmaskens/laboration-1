8726595dc1b053cbbd314935fed446b95a17f996
Freja <corbermann@gmail.com>
2023-01-22 15:12:07 +0100

    Car is now beefy

--- Saab95.java
+++ Saab95.java
@@ -1,13 +1,8 @@
 import java.awt.*;
 
-public class Saab95 extends Car implements Movable{
+public class Saab95 extends Car{
 
     public boolean turboOn;
-    private double xPosition;
-
-    private double yPosition;
-
-    private double direction;
 
     public Saab95(double xPosition, double yPosition, double direction){
         super(2, Color.red, 125, "Saab95", xPosition, yPosition, direction);
@@ -32,32 +27,4 @@ public class Saab95 extends Car implements Movable{
         return enginePower * 0.01 * turbo;
     }
 
-    public void incrementSpeed(double amount){
-        currentSpeed = clamp(getCurrentSpeed() + speedFactor() * amount,0, enginePower);
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = clamp(getCurrentSpeed() - speedFactor() * amount,0, enginePower);
-    }
-    
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(clamp(amount, 0, 1));
-    }
-
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(clamp(amount, 0, 1));
-    }
-
-    public void move() {
-        xPosition += Math.cos(direction) * getCurrentSpeed();
-        yPosition += (-Math.sin(direction)) * getCurrentSpeed();
-    }
-    public void turnLeft(){
-        direction = Math.abs((direction + (Math.PI / 180)) % (Math.PI * 2));
-    }
-    public void turnRight(){
-        direction = Math.abs((direction - (Math.PI / 180)) % (Math.PI * 2));
-    }
 }
